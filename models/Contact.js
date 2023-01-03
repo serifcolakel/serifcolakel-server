959a95c0258899e0295f382208dbb9ef6080b105
serifcolakel <serifcolakel0@gmail.com>
2023-01-03 23:54:37 +0300

    ADDED (serif) : date added in Contact models.

--- models/Contact.js
+++ models/Contact.js
@@ -20,6 +20,10 @@ const contactSchema = new ContactSchema({
     type: String,
     required: true,
   },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model("contact", contactSchema);

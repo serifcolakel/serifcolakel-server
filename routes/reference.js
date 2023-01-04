9304ddac6e7dc29ca7ff2512ce6a55ea02c6093e
serifcolakel <serifcolakel0@gmail.com>
2023-01-04 23:14:09 +0300

    UPDATED (serif) : reference routes updated for new changes.

---
+++ routes/reference.js
@@ -0,0 +1,16 @@
+import express from "express";
+import {
+  createReference,
+  deleteReference,
+  getAllReference,
+  updateReference,
+} from "../controllers/reference.js";
+
+const referenceRouter = express.Router();
+
+referenceRouter.get("/", getAllReference);
+referenceRouter.post("/", createReference);
+referenceRouter.post("/update", updateReference);
+referenceRouter.post("/delete", deleteReference);
+
+export default referenceRouter;

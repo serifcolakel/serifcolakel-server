dc1b2afbe634586a8f7d109c8c1353650e850b6b
serifcolakel <serifcolakel0@gmail.com>
2023-01-07 03:23:58 +0300

    ADDED (serif) : languages routes added for new changes.

---
+++ routes/language.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createLanguage, deleteLanguage, getAllLanguage, updateLanguage } from "../controllers/language.js";
+
+const languageRouter = express.Router();
+
+languageRouter.get("/", getAllLanguage);
+languageRouter.post("/", createLanguage);
+languageRouter.post("/update", updateLanguage);
+languageRouter.post("/delete", deleteLanguage);
+
+export default languageRouter;

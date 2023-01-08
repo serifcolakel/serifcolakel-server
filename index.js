e5e7034da0d12d0384b099c2cdc836014de78a57
serifcolakel <serifcolakel0@gmail.com>
2023-01-08 05:14:50 +0300

    ADDED (serif) : /assets route added for static image after upload image. ADDED (serif) : !fs.existsSync("public/assets") add public/assets static images file.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 import express from "express";
+import fs from "fs";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+import helmet from "helmet";
+import morgan from "morgan";
+import bodyParser from "body-parser";
 
 import authRouter from "./routes/auth.js";
 import userRouter from "./routes/user.js";
@@ -14,25 +20,24 @@ import { verifyToken } from "./middlewares/verifyToken.js";
 
 const app = express();
 
-const PORT = process.env.PORT || 5001;
-
+// Config
 dotenv.config();
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(morgan("common"));
 
-async function connect() {
-  try {
-    await mongoose.connect(process.env.MONGO_DB_URL, {
-      useNewUrlParser: true,
-    });
-    console.log("Mongo DB connected.");
-  } catch (error) {
-    console.error(error);
-  }
-}
+// Global variables
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const MONGO_DB_URL = process.env.MONGO_DB_URL;
+const PORT = process.env.PORT;
 
-connect();
-
+// Routes
+app.use("/assets", express.static(path.join(__dirname, "public/assets")));
 app.use("/auth", authRouter);
 app.use("/user", verifyToken, userRouter);
 app.use("/contact", verifyToken, contactRouter);
@@ -40,4 +45,19 @@ app.use("/reference", verifyToken, referenceRouter);
 app.use("/language", verifyToken, languageRouter);
 app.use("/skill", verifyToken, skillRouter);
 
-app.listen(PORT, () => console.log(`Server started on ${PORT}`));
+// Connect to DB and start server
+mongoose
+  .connect(MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    if (!fs.existsSync("public/assets")) {
+      console.log("Creating assets folder");
+      fs.mkdirSync("public/assets", { recursive: true });
+    }
+    app.listen(PORT, () =>
+      console.log(`DB connected, Server running on ${PORT} PORT`)
+    );
+  })
+  .catch((error) => console.log(`${error} did not connect`));

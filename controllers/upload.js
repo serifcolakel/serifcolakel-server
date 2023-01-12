500f128b48eab4e2a99db0b0264829e7603ca33a
serifcolakel <serifcolakel0@gmail.com>
2023-01-13 00:08:52 +0300

    ADDED (serif) : upload js controllers addewd for single & multiple image upload.

---
+++ controllers/upload.js
@@ -0,0 +1,35 @@
+import { RESULT } from "../common/constants.js";
+
+export const handleUploadSingleImage = (req, res) => {
+  if (!req.file) {
+    return res.status(400).send({
+      result: RESULT.ERROR,
+      message: "Please upload an image",
+    });
+  }
+
+  res.status(200).send({
+    result: RESULT.SUCCESS,
+    message: "Upload image successfully",
+    data: {
+      image: req.file.path,
+    },
+  });
+};
+
+export const handleUploadMultipleImages = (req, res) => {
+  if (!req.files || !Array.isArray(req.files) || req.files.length === 0) {
+    return res.status(400).send({
+      result: RESULT.ERROR,
+      message: "Please upload an image",
+    });
+  }
+
+  res.status(200).send({
+    result: RESULT.SUCCESS,
+    message: "Upload images successfully",
+    data: {
+      images: req.files.map((file) => file.path),
+    },
+  });
+};

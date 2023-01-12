f37ec5b0e89990385acaad70db8b8b9783a053cb
serifcolakel <serifcolakel0@gmail.com>
2023-01-13 00:10:03 +0300

    REMOVED (serif) : handleUploadAsset mw removed.

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,10 @@
 import express from "express";
 
 import { handleLogin, handleRegister } from "../controllers/auth.js";
-import { handleUploadAsset } from "../middlewares/handleUploadAsset.js";
 
 const authRouter = express.Router();
 
-authRouter.post("/register", handleUploadAsset, handleRegister);
+authRouter.post("/register", handleRegister);
 authRouter.post("/login", handleLogin);
 
 export default authRouter;
